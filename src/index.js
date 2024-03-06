820976486eba794f674e89196d8b14b51f8d1b9e
mrdavehun21 <ernhofferd@gmail.com>
2024-03-06 21:01:20 +0100

    mongodbfirst

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-const express=require("express");
-const app=express();
-const path=require("path");
+const express=require("express")
+const app=express()
+const path=require("path")
+const hbs=require("hbs")
+const collection=require("./mongodb")
 
-app.listen(3000, ()=>{
+const templatePath=path.join(__dirname, '../templates')
+
+app.use(express.json())
+app.set("view engine","hbs")
+app.set("views", templatePath)
+
+app.get("/",(req,res)=>{
+    res.render("login")
+})
+app.get("/",(req,res)=>{
+    res.render("signup")
+})
+
+app.post("/signup",async (req,res)=>{
+
+    const data={
+        name:req.body.name,
+        password:req.body.password
+    }
+
+    await collection.insertMany([data])
+
+    res.render("home") //redict to home
+
+})
+
+
+app.listen(3000,()=>{
     console.log("port connected");
 })
 
