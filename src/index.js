8bbbc6ac4c47787656f212bfe907d9593c364672
mrdavehun21 <ernhofferd@gmail.com>
2024-03-13 21:55:01 +0100

    working on showing hev

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
+const { Hev, Collection } = require("./mongoDB")
 const app = express();
-const { Hev, collection } = require("./mongodb");
 
 app.use(express.json());
 app.set("view engine", "ejs");
-//app.set("views", templatePath);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
@@ -21,14 +20,42 @@ app.get("/signup", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-    Hev.find().then((data) => {
+    res.render("home");
+});
+
+app.get("/home/vonatok", (req, res) => {
+    Hev.find({}, { _id: 0 }).then((data) => {
         console.log(data); // Check if data is fetched correctly
-        res.render("home", { hev: data }); // Pass movies data to your home template
+        
+        // Log the structure of the first document
+        if (data.length > 0) {
+            console.log("Structure of the first document:", data[0]);
+        }
+        
+        // Extract numbers from the 'szerelveny' arrays
+        const numbers = [];
+        data.forEach(item => {
+            item.vonalak.forEach(vonal => {
+                vonal.vonatok.forEach(vonat => {
+                    vonat.szerelveny.forEach(number => {
+                        numbers.push(number);
+                    });
+                });
+            });
+        });
+        
+        // Log the extracted numbers
+        console.log("Extracted numbers:", numbers);
+        
+        // Render the "vonatok" template
+        res.render("vonatok", { vonatokNumbers: numbers });
     }).catch((error) => {
         console.error("Error:", error);
         res.status(500).send("Error");
     });
 });
+
+
 
 
 app.post("/signup", async (req, res) => {
@@ -38,7 +65,7 @@ app.post("/signup", async (req, res) => {
     };
 
     try {
-        await collection.create(data);
+        await Collection.create(data);
         console.log("User created successfully:", data);
         res.redirect("/login");
     } catch (error) {
@@ -50,7 +77,7 @@ app.post("/signup", async (req, res) => {
 
 app.post("/login", async (req, res) => {
     try {
-        const check = await collection.findOne({ name: req.body.name });
+        const check = await Collection.findOne({ name: req.body.name });
 
         if (check && check.password === req.body.password && req.headers.referer.includes('/login')) {
             res.redirect("/home");
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,35 +1,51 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://mrdavehun:123@palyaszamok.92qbrfa.mongodb.net/")
-.then(()=>{
+.then(() => {
     console.log("mongodb connected");
 })
-.catch((e)=>{
+.catch((e) => {
     console.log(e);
-})
+});
 
-const LogInSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const LogInSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     }
-})
+});
 
-const HevsSchema = {
-    azon: String,
-    palyaszam: String,
-    kekbelso: String,
-    leaderToBatthy: String
-}
+const Collection = new mongoose.model("loginCollection", LogInSchema);
 
-const Hev= mongoose.model('hevCollection', HevsSchema);
-const collection=new mongoose.model("loginCollection",LogInSchema);
+//-------------------------------------------------------//
+
+const VonatSchema = new mongoose.Schema({
+    irany: {
+        type: String,
+        required: true
+    },
+    szerelveny: {
+        type: [Number],
+        required: true
+    }
+});
+
+const VonalSchema = new mongoose.Schema({
+    szam: {
+        type: Number,
+        required: true
+    },
+    vonatok: [VonatSchema]
+});
+
+const HevModel = mongoose.model('hevcollections', VonalSchema);
+
 
 module.exports = {
-    Hev: Hev,
-    collection: collection,
-  };
+    Hev: HevModel,  //index.js-ben lévő const megnevezés, ebben a fájlban lévő megnevezés
+    Collection: Collection,
+};
