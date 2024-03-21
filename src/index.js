939632154bf340ccc0f881ba5fc0aa107ad618e0
mrdavehun21 <ernhofferd@gmail.com>
2024-03-21 20:58:38 +0100

    more styling reqiuired data shown correctly

--- src/index.js
+++ src/index.js
@@ -58,6 +58,7 @@ app.get('/home/vonatok', async (req, res) => {
             tripId: vehicle.tripId,
             stopId: vehicle.stopId,
             licensePlate: vehicle.licensePlate,
+            status: vehicle.status,
             tripHeadsign: matchTripIdToTripHeadsign(vehicle.tripId, vehiclesData.data.references.trips),
             stopName: matchStopIdToName(vehicle.stopId, vehiclesData.data.references.stops)
         }));
@@ -69,12 +70,33 @@ app.get('/home/vonatok', async (req, res) => {
 
         const stopPromises = [];
 
+        function getCurrentDate() {
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
+            const day = now.getDate().toString().padStart(2, '0');
+            return `${year}${month}${day}`;
+        }
+        function getDateForDesign(){
+            const now = new Date();
+            const options = {
+                hour12: false,
+                hour: '2-digit',
+                minute: '2-digit',
+                weekday: 'short',
+                month: 'long',
+                day: 'numeric'
+            };
+            return now.toLocaleString('en-US', options);
+        }
+        const currentDate = getCurrentDate();
+        const dateForDesign = getDateForDesign();
         // Iterate over each vehicleId and make a request
         for (const vehicle of vehicles) {
             const stopPromise = axios.get('https://futar.bkk.hu/api/query/v1/ws/otp/api/where/trip-details', {
                 params: {
                     vehicleId: vehicle.vehicleId,
-                    date: '20240321',
+                    date: currentDate,
                     ifModifiedSince: 1625685137,
                     appVersion: '1.1.abc',
                     version: 4,
@@ -102,13 +124,13 @@ app.get('/home/vonatok', async (req, res) => {
             const stopsData = stopTimesData.data.entry.stopTimes.map(stop => ({
                 stopId: matchStopIdToName(stop.stopId, stopTimesData.data.references.stops),
                 stopHeadsign: stop.stopHeadsign,
-                arrivalTime: new Date(stop.arrivalTime * 1000).toLocaleTimeString('hu-HU', { hour: '2-digit', minute: '2-digit', hour12: false })
+                arrivalTime: new Date(stop.arrivalTime * 1000).toLocaleTimeString('hu-HU', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false }),
+                predictedArrivalTime: new Date(stop.predictedArrivalTime * 1000).toLocaleTimeString('hu-HU', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false })
             }));
 
             // Store stops data indexed by vehicleId
             stopsByVehicle[vehicleId] = stopsData;
         }
-        
 
         /*
         No stopName for every stop        
@@ -121,7 +143,7 @@ app.get('/home/vonatok', async (req, res) => {
         });
         */
         // Render the EJS file with the data
-        res.render('vonatok', { vehicles, stopsByVehicle });
+        res.render('vonatok', { vehicles, stopsByVehicle, currentDate, dateForDesign });
         } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).send('Error fetching data');
