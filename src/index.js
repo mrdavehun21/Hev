fdd7e71755fba383591ef3c69d4190f8d0cced26
mrdavehun21 <ernhofferd@gmail.com>
2024-06-14 11:24:09 +0200

    Viewable vehciles on map, TEMP UPDATE

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const axios = require('axios');
 const fs = require('fs');
 const rateLimit = require('express-rate-limit');
 const path = require('path');
+var polyUtil = require('polyline-encoded');
 
 app.use(express.json());
 app.use(session({
@@ -96,7 +97,11 @@ async function fetchDataFromAPIH5() {
             status: vehicle.status,
             label: vehicle.label,
             tripHeadsign: matchTripIdToTripHeadsign(vehicle.tripId, vehiclesData.references.trips),
-            stopName: matchStopIdToName(vehicle.stopId, vehiclesData.references.stops)
+            stopName: matchStopIdToName(vehicle.stopId, vehiclesData.references.stops),
+            location: {
+                lat: vehicle.location.lat,
+                lon: vehicle.location.lon
+            }
         }));
 
         const stopsByVehicle = {};
@@ -121,13 +126,21 @@ async function fetchDataFromAPIH5() {
                 predictedArrivalTime: formatTime(stop.predictedArrivalTime),
                 predictedDepartureTime: formatTime(stop.predictedDepartureTime),
                 arrivalTime: formatTime(stop.arrivalTime),
-                departureTime: formatTime(stop.departureTime)
+                departureTime: formatTime(stop.departureTime),
             }));
         });
 
+        var encoded1 = "us}`HisesB{A?_A@mA?o@?eA?{CAw@?yCCs@?_A?mAAg@?u@?_CI_ACmAC]AUCU?QAuAHG@[DUD]DYB[BO@M?O?O?MAGAS?OAMAg@Co@Ci@CMAK?K?I?M@M@I@I@K@GBsBb@kMxB??o@Jc@Jy@Le@@}@DI?E?U?W@W?W?YAY?[A[AYA[CYA[CYCYEYCYEYEYEYGWEWGWGWGWIWGo@SUIWIUIUGWIUIWIYIWGWIWGWIWGWGUGWIUGmAYSEQESEUEUGWGUECAYGYGYGWGYGYGWGYGWGWGYGqA_@UIUKUMUKUOUMWOYOWOYQYOYQiEwCUQWQUQk@c@UQUQSSUSUSUSUUUUWUUWWSkAkAUSUUCCOOUSSSUSSSSSSSSSQSSUQUQUSYSWUWUWUSUSUQWMUMWKWIWGWGYGYGYG]G[E[E[E]E]A[C]?]A]@Y?W?U@U@gDFY@U@W@W?Y@W@W?U@W@U?W@YBYB[BYB[B]B[D[B[B[BYB[@[@Y@Y@Y?Y?Y?Y?WAY?W?W?WAW?U?W?WAY?[?[AYA[C[CYE[EYGYIYGWKYIWIYKWIWKWIWIWIWGWEWEWCYAYAYAYAY?YAY?Y@W?Y@W?Y@WBW@WDUDWDWFWFUHWJWJULULWNSNQJUPQLSLQLSLQLUNOLSLGDMHUPWNWPWPWPWPWPWPWRYPWPYPYPYLYLYJmA^YBYDY@g@@s@?SAQAYEWCSEQEQESEUIUIQISKSKUMWMGEMIWOYOWMWMWMUKWKUIWIUGWIWGYIYIYIYIYIYGYIYIWGWIWGWIWGWGUIYGWIYIYGWIWIWGWGWIUGWGUGUIWGWGWIYIYGYIYIYIYIWGYIWIYGWIWGWIWGWGUIWGWIYGYIYIYIYIWGYIYGWIYEYGWEYC_AI[As@?W@S@S?YBSBUBSBK@E@SDUDQDSFUFWHWHYHYH[HYFYFYD[DYBYDWDYDYDWBYDWDWBWDcALs@BY?Y?YCYAYEYEWEWCYEWEWEWEWE[CYC[CYC[CYCYAYCYCYCYCYCYCWAWCUCSASASCQAUCUASCQAWCIAOAQAUCWAWCYC[CYCYCYCYCYCYCWAWAW?W?W@WBYBYD[FYFYHWJYLWLWNWNWPURUPWRUPWRWTWRWTWRWRWRWTWRURWRUPWRURUPURUPURUPUPSPUPSPUPSPSPSNSPSNSNSNSNSLSJi@TUHUFUFWBUDU@W@U?U?UCSCSEUEQGSIQISKQKOMQKQMQKSOUOUOWQYQWQIEOKWQYSWQWQYQWQYQWQWQWQYQWQWQWOUQWQWOUQUOWOUOUQUOSOUMUOSOUOSMSMSOUOUOWQWQUSUSUUSWSWQWQ[QYOYQYQWQWQUQSQSSQSOQOSOUMSQWOUQWQWQWQWQWQWQUQWOUQWQUOUOUQQKSOOKOKQKOMQMWQSMSOUOWQUOUQWQUOUQUQUQUQUQSQUOUQSOUMUOSOUMSOUMSOSMCAOKSMSMSMSOa@WUOWQWQYQWOYQWQWQWOWQUOWQUOWOUOUOWOSOUOUOUMSOUOUOUOWOWQYQWQYQWOWOWMWKWKWIWGWEYEWEYC[C[C[C]C[E[C[C[Ck@E[C[E[C]C[Ck@G{@G]CuAMYC[CYCYCYCYCYCYCYCYAYAYAW?Y?Y?W@YBW@YDWBYFWDWFc@JWHUHWJUJWJUJUJWJUJWJULUJUJUJUJUJUHUJSJUHIDg@R]L]LYHwAf@y@VA@y@VYJ{@Xq@Tc@POHSJIDIFIDGDC@CBCBC@CBEDKJMLYTIJgCjC]\\y@z@QRIJKJSXUXORQVi@`AOTi@bAo@jAm@fAa@t@m@hAg@|@m@hAKNOZSZQZQZSZQXQZSZQZQZQZQXQZOXOXOXOVOXOVOXMVOVOVMVOVOVOTOVOTQVQVUVEFMNUVUTUTWTURc@ZWPWPYNWLYNYJYLYJYH[H[HYF[H[Hg@JYHYFYFYFYFYHWFYFYFWFYHWFYFWFYHWHWHWJWHWJWJWHWJWJWHUJWJUHWHUJUHUHUJMDGBUHUHUJWHWJYJYJYJYLWJWHWJWHUJUHWJWHWJWJYJWJYJWJYJWJYJYJYJYJWJYJa@PWHUHWJWJWHWJWJWJYJWJYJYJWJYJYJYJYJYJWJYLYJYJYJYJWJYJYJWLYLe@RWNWNYNWNYLYPYNWNWNWLWLa@TWNWLWNWNWNYNc@Ve@VWNWNULUPUPUPURUTUTSVUVmAhBOXQXMZOVMXMVMXMVMXOXMXMXMVMXMVKVMTS`@KTKTKRKRQ^O\\OZOZKTKTKTGLCFKTKRKTIROXITMTMXOZOXQX[`@SRUPWPULe@NYHWH[Fg@HY@[?[CWEa@KUIUMUOSQSSSSQWMUMWKWK[KYI]I_@Gc@Ge@Cg@Ag@Ag@@i@Bi@Bg@Dk@Fg@Be@Dg@De@De@De@De@Fy@Bg@Bg@@e@@i@?g@?g@?e@Ag@Ae@Ag@Ce@Ce@G{@Ge@Gg@Ge@Mw@Ga@Ic@Mq@Ia@I_@Ga@Ic@Ia@Ow@Ic@Ke@Ie@Ke@Ig@Ou@Ic@Ic@Ic@Ic@Ic@Ic@Ow@Ic@Ic@Kc@Mu@Ic@Ic@Ic@Os@Ic@Ia@Ic@Ic@Ia@Ic@Ic@Ia@Gc@Ia@Mu@Gc@Gc@Gc@Ec@Iw@Ee@Ec@Ce@Ce@Ce@Ag@Ae@Ag@A}@?i@?g@?i@@i@@}@?i@@y@?e@@e@?e@@w@@e@?c@@e@?w@@c@?e@@w@@c@?e@@u@?c@@e@?c@@c@@u@?c@?a@@c@?a@@u@?g@@e@?i@?g@Ag@?i@C{@Ci@Ag@Ci@Ei@Eg@Ei@I}@Gi@Ky@Ie@Ga@Ia@G_@I_@I_@I]Om@K_@K]K]M]Sk@M]M[M]O[g@cAOYOYQWWc@QWQUYa@QUQUSUQUQS]a@KMEGQUSUQUSUQUQS[a@QSQU[_@QSQSOUQSY]QSY]QSOSQU[_@SUSWSWUYSWUWSWUW]e@UWSUSWSWUW]c@SUSWSW[a@SUSW[_@SUQU[_@QUOSY]OQOSOQOQMOSUCEo@w@OSg@m@IKMOIMIKIMc@q@_DwEm@}@Wa@qBwC}A_C"
+        var encoded2 = "_yeaHircsBdA@P?F?H?`@@h@AX?PARAb@Cn@KVGtCe@nAU|@STEDAdBUvAU|Bc@@JAKHAB?f@IXGvAU~B_@jASn@M@?f@Od@Mn@SPGjA_@@LAMBABAHCPENG^Ml@Sv@Wf@OzAg@ZKRIJEBAb@Sl@]\\Sn@]j@]j@[h@[b@YrA}@|@k@@HAIDAFEHGPMFC`As@bCiB|AiA|@q@HGrAcAf@c@DCNQFIX_@n@y@h@s@T[`@k@@?DJEKDGVUZUBCNIPIrAc@`Bi@VItAc@^Ih@IpBKfC^nC^f@Hb@D?H?I@@@?L@H@H@r@LrBVrGv@F@xBVRBH@T@lA?B??B?CH?|GEhA?F?~@Aj@?L@H?J@P@j@Ff@HPBfARHB?@?APBNBv@LnAPlARz@N^BZ@tA@h@?P?J?VENCNELEFCLE`Ai@LG?F?GPIj@WLI@?rAWpBWZEDCDADCDE@ABE@EBGBKBSAQ?IMaBAM?I?Q?M?EPqBJDKEBQ@I@K?A@C?E@CBE@GBCDGBEDEDC@?BCB?BAB?B?t@AB?FAJCFE^SFCHCF?DAH?D@R?j@DL@F@F@NDB?HBF@F@D@jALhALbAD`AFB??H?IL@D?xBDtC@H?D?rDB`DCtCGH?dIAfAGF??J?K~@C\\CfAE@?ZAdF]TApDa@z@K?F?GFA`@GZE|AW`Ca@xCo@pA[f@Oh@OLEHCFCFCFCBADAFAD?B@@@B@DBBDDDBJBHDNBHBFBB@@DBFBH@F?F?HEDCDEBGBE@EBE@G@I@M?M@U@K@KBKDIDGFGDCFEFCZOFCz@]HEBHCIBAlAg@rCsA@A@AtCsArAm@bAs@f@_@hDeCJI~@s@HGDLEMDCFE~BgBbAw@fCmBNKFEVQ^SLILIJIFINSP[PUb@e@\\c@LM|@gABCDHCGHIbFwFLONQPMNKRKRMHEFGHGFIHKn@mABGJONQvDcEzCgDtA_BHKHILMPKLGn@YHEFEFEFGFELONSbAyABEBCBCFCJED?H?H@FDHDx@|@DDDBFD@@AD@EBBPHDBB@LF^JxA`@j@PJBXHLDB@XHRDrA`@XJZLTJVL\\RDBl@\\RJVPf@\\B@AD@EFFDBPNHHl@l@PRFJHLFJHNhBrDJPHRHTL^`@tAh@fBFRDLJ^BFZrADLBFBDBDFFHDF@L@@?`@BjBN?B?C@?`@DB?\\D\\DTFTDHBrA\\bCl@PDJBf@L`@J?A?@F@\\JPDRHJBD@PDRF^HfAX`@JNBLBLBL@h@L~ElAPFB?hBb@bAXb@JRD|@VPDHB?@?ALBbDx@v@TbAVpD~@FB?B?CB?VHVF~@T~Bl@`Bb@pD~@fAXdAXfAXRHRHNDND~Bj@D@@EADHBF@D@FBJBF@PD\\Hp@P~@Tz@Rr@Pz@Tr@Rx@RhCp@H@VHXH?@?AD@JBVFNDB@|A`@jBd@xIzB?F?GJDZHVFzKrChBd@JDXFdAXdBd@AL@MF@DBLFJFPLDDHDHDHBJDHBdBZPBLBVD~ALxBR|AL`E^B?NBZBjAJD?AJ@KzD^jBPF?P@dK~@H@VBjAJD@AH@IxGl@jBPPBB??F?GVBL@PBnAL^BZBH@lADjADZ@X?NAPANCFAFANERGPG\\Oz@_@@Ah@U|DeBf@STGTAN@NBLDNHLLPPnB|BZh@DLDLDTDZBX?BA?@?@V?ZAVAREVEXK\\IRORMPQP_@T[NYP]XUPQRQTU^c@x@c@`AoDlIWp@Od@CFKb@Ol@AH?A?@Mt@Ib@Eb@IfASxCMfAMfAQbASdAU~@Wz@Sl@Uj@wH|QAA@@?@GNQb@Sh@YbAWz@WdASfAQhAUzA[fCeAlIFBGCAB_Ex[Ir@K`AIhAAJ?B????AJ?LC\\CdAAb@Ab@?t@?L@pC?lAAVEdBIxAGr@Ix@It@Kt@Kr@uBfLA?@?CFQ`ASfAYfBSzAMdAIx@MvAGhAEdAEtACxA?lB@tA@xA?????D?F@pB@lAAnA?vAOjUItLArA?DA?@??@?`BBfBFnBJ|ALhBJdALdAPjAXdBXpAC@BA??H\\FZJ`@HZLd@\\jAZfAFRHTJTHRHNHLHLHJNRLLVV`@\\bAz@bDrCZZHHHHFJLPJTFLLZDNXv@BLDNDRDRDT?@BT@RBZFtA@NBn@@\\DbAB\\FtA??A@"
+        var latlngs = polyUtil.decode(encoded1);
+        var latlngs2 = polyUtil.decode(encoded2);
+
         app.locals.vehiclesH5 = vehicles;
+        app.locals.latlngs = latlngs;
+        app.locals.latlngs2 = latlngs2;
         app.locals.stopsByVehicleH5 = stopsByVehicle;
         app.locals.currentDateH5 = getCurrentDate();
+        
     } catch (error) {
         console.error('Error fetching data:', error);
     }
@@ -167,6 +180,131 @@ app.post('/home/vonatokH5', (req, res) => {
 
 //----------------------Vonatok_END-------------------------------------------//
 
+//----------------------Villamosok-------------------------------------------//
+
+//--------------17,19,41------------------//
+//const routeIds = ['BKK_3410', 'BKK_3190', 'BKK_3170'];
+// Function to fetch data from API
+async function fetchDataFromAPIVillamos(routeIds) {
+    try {
+        const apiKey = process.env.API_KEY;
+        const allVehicles = [];
+        const allStopsByVehicle = {};
+
+        for (const routeId of routeIds) {
+            const vehiclesResponse = await axios.get('https://futar.bkk.hu/api/query/v1/ws/otp/api/where/vehicles-for-route', {
+                params: {
+                    routeId: routeId,
+                    related: false,
+                    version: 2,
+                    includeReferences: true,
+                    key: apiKey
+                }
+            });
+
+            const vehiclesData = vehiclesResponse.data.data;
+            const vehicles = vehiclesData.list.map((vehicle, index) => ({
+                index: index + 1,
+                vehicleId: vehicle.vehicleId,
+                tripId: vehicle.tripId,
+                stopId: vehicle.stopId,
+                licensePlate: vehicle.licensePlate,
+                stopSequence: vehicle.stopSequence,
+                status: vehicle.status,
+                label: vehicle.label,
+                tripHeadsign: matchTripIdToTripHeadsign(vehicle.tripId, vehiclesData.references.trips),
+                stopName: matchStopIdToName(vehicle.stopId, vehiclesData.references.stops),
+                location: {
+                    lat: vehicle.location.lat,
+                    lon: vehicle.location.lon
+                }
+            }));
+
+            allVehicles.push(...vehicles);
+
+            const stopPromises = vehicles.map(vehicle =>
+                axios.get('https://futar.bkk.hu/api/query/v1/ws/otp/api/where/trip-details', {
+                    params: {
+                        vehicleId: vehicle.vehicleId,
+                        date: getCurrentDate(),
+                        version: 4,
+                        includeReferences: true,
+                        key: apiKey
+                    }
+                })
+            );
+
+            const stopResponses = await Promise.all(stopPromises);
+
+            stopResponses.forEach((stopResponse, i) => {
+                allStopsByVehicle[vehicles[i].vehicleId] = stopResponse.data.data.entry.stopTimes.map(stop => ({
+                    stopId: matchStopIdToName(stop.stopId, stopResponse.data.data.references.stops),
+                    stopSequence: stop.stopSequence,
+                    predictedArrivalTime: formatTime(stop.predictedArrivalTime),
+                    predictedDepartureTime: formatTime(stop.predictedDepartureTime),
+                    arrivalTime: formatTime(stop.arrivalTime),
+                    departureTime: formatTime(stop.departureTime)
+                }));
+            });
+        }
+
+        app.locals.vehiclesVill = allVehicles;
+        app.locals.stopsByVehicleVill = allStopsByVehicle;
+        app.locals.currentDateVill = getCurrentDate();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function startFetchingDataVillamos() {
+    const routeIds = ['BKK_3410', 'BKK_3190', 'BKK_3170'];
+    await fetchDataFromAPIVillamos(routeIds);
+    setInterval(() => fetchDataFromAPIVillamos(routeIds), 30000);
+}
+
+// Start fetching data
+startFetchingDataVillamos();
+
+// Routes
+app.get('/home/villamos', (req, res) => {
+    try {
+        const vehiclesVill = app.locals.vehiclesVill || [];
+        const stopsByVehicleVill = app.locals.stopsByVehicleVill || {};
+        // const selectedDirectionVill = req.session.selectedDirectionVill || '';
+        // , selectedDirectionVill
+        res.render('villamos', { vehiclesVill, stopsByVehicleVill });
+    } catch (error) {
+        console.error('Error rendering page:', error);
+        res.status(500).sendFile(path.join(__dirname, 'public', '500.html'));
+    }
+});
+/*
+app.post('/home/villamos', (req, res) => {
+    try {
+        req.session.selectedDirectionVill = req.body.selectedDirectionVill;
+        res.redirect('/home/Villamos');
+    } catch (error) {
+        console.error('Error processing filter:', error);
+        res.status(500).sendFile(path.join(__dirname, 'public', '500.html'));
+    }
+});
+*/
+//----------------------Villamosok_END-------------------------------------------//
+app.get("/home/map", (req, res) => {
+    const vehiclesH5 = app.locals.vehiclesH5 || [];
+    const latlngs = app.locals.latlngs || [];
+    const latlngs2 = app.locals.latlngs2 || [];
+    const vehiclesVill = app.locals.vehiclesVill || [];
+    const licensePlatesData = JSON.parse(fs.readFileSync('./data/licensePlates.json', 'utf8'));
+
+    if (req.xhr) {
+        // If the request is an AJAX request, respond with JSON data
+        res.json({ H5: vehiclesH5, tram: vehiclesVill, latlngs: latlngs, latlngs2: latlngs2, licensePlates: licensePlatesData});
+    } else {
+        // Otherwise, render the EJS template
+        res.render('map', { H5: vehiclesH5, tram: vehiclesVill, latlngs: latlngs, latlngs2: latlngs2, licensePlates: licensePlatesData});
+    }
+});
 
 //----------------------Bejelentkezés, Regisztráció------------------//
 app.get('/logout', (req, res) => {
